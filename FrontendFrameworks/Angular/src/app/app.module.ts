5a95a629f21d2cdb7e6776bf33e88d62220ed57c
Tobias <tobias0605@googlemail.com>
2021-10-21 13:24:18 +0200

    angular test

--- FrontendFrameworks/Angular/src/app/app.module.ts
+++ FrontendFrameworks/Angular/src/app/app.module.ts
@@ -1,19 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {Card} from '../../../../Views/src/my-card';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [Card],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
